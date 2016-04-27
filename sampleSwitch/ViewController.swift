f13de93f67ba70e6bce7d29f2cdd226f00080352
AI Matsubara <aiai.1116.info@gmail.com>
2016-04-27 15:43:36 +0800

    initial commit

--- sampleSwitch/ViewController.swift
+++ sampleSwitch/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func switchChanged(sender: UISwitch) {
+        print("切り替わりました")
+        
+        //sender:イベントを発生させている部品そのもの
+        print(sender.on)
+        
+        //TODO:スイッチがONのときは「スイッチオン」、OFFのときは「スイッチオフ」とデバックエリアに表示するif文を作ってみましょう
+        
+        //if sender.on == trueと書いてもいい
+        //ブーリアン型？？
+        //sender.offはない、なぜ？？
+        if sender.on {
+            print("スイッチオン")
+        } else {
+            print("スイッチオフ")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
